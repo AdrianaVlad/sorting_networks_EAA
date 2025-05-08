297bfce3c82c74ff214f5096e38d4a2f39d1e84e
ioanacioata02 <100218582+ioanacioata02@users.noreply.github.com>
2025-05-08 19:23:12 +0300

    Revert "Add files via upload"
    This reverts commit 6217e84ccc6d0e803a91952141987400d65efa3a.

--- FitnessEstimator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Network;
-
-class FitnessEstimator {
-public:
-    virtual double compute(const Network* net) const = 0;
-    virtual ~FitnessEstimator() = default;
-};
--- FitnessOutputSize.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "FitnessEstimator.h"
-#include "Network.h"
-
-class FitnessOutputSize : public FitnessEstimator {
-public:
-    double compute(const Network* net) const override;
-};
